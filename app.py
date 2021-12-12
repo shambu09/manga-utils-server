7df00f53161aba1ba31bdda56fea53207537a667
shambu <shambuqwerty@gmail.com>
2021-12-12 15:35:18 +0530

    Added mongodb support for faster fetch, CORS decorator Implemented

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import logging
+import os
+from functools import wraps
+import re
 
-import requests
-from flask import Flask, make_response, request
-
-get_public_url_file = lambda file_id: f"https://drive.google.com/uc?export=view&id={file_id}"
+from dotenv import load_dotenv
+from flask import Flask, jsonify, make_response, request
+from flask_pymongo import PyMongo
 
 
 def init_logger(app):
@@ -11,43 +13,85 @@ def init_logger(app):
     app.logger.handlers.extend(gunicorn_error_logger.handlers)
     app.logger.setLevel(logging.DEBUG)
 
+
+def cross_orgin(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if request.method == 'OPTIONS':
+            response = make_response()
+            response.headers.add("Access-Control-Allow-Origin", "*")
+            response.headers.add('Access-Control-Allow-Headers', "*")
+            response.headers.add('Access-Control-Allow-Methods', "*")
+            return response
+
+        resp = func(*args, **kwargs)
+        resp.headers.add("Access-Control-Allow-Origin", "*")
+        return resp
+
+    return wrapper
+
+
 def create_app():
+    load_dotenv()
     app = Flask(__name__)
+    app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+    mongo = PyMongo(app)
+    db = mongo.db
     init_logger(app)
 
-    @app.route('/')
+    @app.route('/', methods=['GET', 'OPTIONS'])
+    @cross_orgin
     def index():
-        return """<h1>Manga Crawler Server</h1><a href="https://github.com/shambu09/manga-crawler">Repo</a>"""
+        return make_response(
+            """<h1>Manga Crawler Server</h1><a href="https://github.com/shambu09/manga-crawler">Repo</a>""",
+            200)
 
-    def _build_cors_preflight_response():
-        response = make_response()
-        response.headers.add("Access-Control-Allow-Origin", "*")
-        response.headers.add('Access-Control-Allow-Headers', "*")
-        response.headers.add('Access-Control-Allow-Methods', "*")
-        return response
+    @app.route("/get_index", methods=["GET", "OPTIONS"])
+    @cross_orgin
+    def get_index():
+        index = db["content"].find_one({"_meta": 0})
+        return make_response(jsonify(index), 200)
 
-    def _corsify_actual_response(response):
-        response.headers.add("Access-Control-Allow-Origin", "*")
-        return response
+    @app.route("/change_index", methods=["PUT", "PATCH", "OPTIONS"])
+    @cross_orgin
+    def change_index():
+        data = request.get_json()
+        if not data:
+            return make_response(jsonify({"message": "No data found"}), 400)
+
+        if request.method == "PUT":
+            if data.get("_id", "no_id") == "no_id":
+                return make_response(jsonify({"message": "No id found"}), 400)
+
+            if data.get("_meta", "no_meta") == "no_meta":
+                data.update({"_meta": 0})
+
+            db["content"].replace_one({"_meta": 0}, data)
+            app.logger.info("Index Regenerated")
+            return make_response(jsonify({"message": "Index Regenerated"}),
+                                 200)
+
+        if request.method == "PATCH":
+            db["content"].update_one({"_meta": 0}, {"$set": data})
+            app.logger.info("Index Altered")
+            return make_response(jsonify({"message": "Index Altered"}), 200)
 
     @app.route('/get_metadata', methods=['GET', "OPTIONS"])
+    @cross_orgin
     def get_file():
-        if request.method == 'OPTIONS':
-            return _build_cors_preflight_response()
-
         file_id = request.args.get('file_id', "")
         resp = {}
 
         if file_id == '':
-            resp = make_response("Error: File not found", 404)
-            return _corsify_actual_response(resp)
+            resp = make_response(jsonify({"message": "No file found"}), 400)
 
         try:
-            resp = requests.get(get_public_url_file(file_id)).json()
+            resp = db["content"].find_one({"_id": file_id})
+            resp = jsonify(resp)
 
         except Exception as e:
             app.logger.error(f"Error: {e}")
-            resp = make_response("Error: File not found", 404)
+            resp = make_response(jsonify({"message": "No file found"}), 400)
 
         else:
             resp = make_response(
@@ -55,11 +99,27 @@ def create_app():
                 200,
             )
 
-        resp = _corsify_actual_response(resp)
         return resp
+
+    @app.route("/add_manga", methods=["PUT", "OPTIONS"])
+    @cross_orgin
+    def put_file():
+        data = request.get_json()
+        if not data:
+            return make_response(jsonify({"message": "No data found"}), 400)
+
+        if data.get("_id", "no_id") == "no_id":
+            return make_response(jsonify({"message": "No _id found"}), 400)
+
+        if data.get("_meta", "no_meta") == "no_meta":
+            data.update({"_meta": 1})
+
+        db["content"].insert_one(data)
+        app.logger.info("Manga Added")
+        return make_response(jsonify({"message": "success"}), 200)
 
     @app.errorhandler(404)
     def Error(error):
-        return "<h1>404 Error: Page Not ound</h1>"
+        return make_response("<h1>404 Error: Page Not Found</h1>", 404)
 
     return app
